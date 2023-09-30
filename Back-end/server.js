7e0b660c6169f4424d316a5abafc2c43d50cc37c
Khanh <21520982@ms.uit.edu.vn>
2023-09-30 16:01:08 +0700

    added routes

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.listen(8000 , ()=>{
-    console.log('Server is running on port 8000')
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}/`);
+});
