9a94c6aa031f0436f34f940d3b33490415a6ec2f
nguyenquochuy753 <92136604+nguyenquochuy753@users.noreply.github.com>
2023-12-05 09:33:15 +0700

    backe-end: update meal in table model

--- Back-end/controllers/table.controller.js
+++ Back-end/controllers/table.controller.js
@@ -43,7 +43,7 @@ const tableController = {
     try {
       await tableModel.findByIdAndUpdate(id, req.body);
       res.status(200).json("updated successfully");
-    } catch (error) {
+    } catch (error) {''
       res.status(500).json(error);
     }
   },
