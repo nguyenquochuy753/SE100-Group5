71162cabdf05929e93243ca560335236cb15f911
nguyenquochuy753 <92136604+nguyenquochuy753@users.noreply.github.com>
2023-12-25 21:14:20 +0700

    update notify in header in cheff

--- Front-end/admin/src/Components/Cheff/Common/Header/Header.jsx
+++ Front-end/admin/src/Components/Cheff/Common/Header/Header.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import './Header.css'
-import logo from "./logo_restaurant-removebg-preview.png";
-import {Link} from "react-router-dom"
+import React from 'react';
+import './Header.css';
+import logo from "../../logo_restaurant-removebg-preview.png";
+import { Link } from "react-router-dom";
+import NotificationBell from '../NotifyBell/NotifyBell';
 
-function Header() {
+function Header({ notificationCount, selectedIngredients }) {
   return (
     <div>
-        <header className="header">
-            <div className="logo">
-                <img src={logo} alt="Logo" />
-            </div>
-            <div className="header-buttons">
-                <button>
-                  <Link to="/orderManagement">Chuẩn bị món ăn</Link>
-                </button>
-                <button>
-                  <Link to="/ingredient">Nguyên liệu</Link>
-                </button>
-            </div>
-        </header>
+      <header className="header">
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="header-buttons">
+          <NotificationBell count={notificationCount} selectedIngredients={selectedIngredients} />
+          <button>
+            <Link to="/orderManagement">Chuẩn bị món ăn</Link>
+          </button>
+          <button>
+            <Link to="/ingredient">Nguyên liệu</Link>
+          </button>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
