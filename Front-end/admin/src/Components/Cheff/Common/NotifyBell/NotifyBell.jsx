3071053116ea6f94e55e3d5a9199f65e7e16eae5
nguyenquochuy753 <92136604+nguyenquochuy753@users.noreply.github.com>
2023-12-25 21:11:39 +0700

    update notify bell item page

--- Front-end/admin/src/Components/Cheff/Common/NotifyBell/NotifyBell.jsx
+++ Front-end/admin/src/Components/Cheff/Common/NotifyBell/NotifyBell.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./NotifyBell.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 
-function NotificationBell({ count }) {
+function NotifyBell({ count, selectedIngredients }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNotifyBell = () => {
+        setIsOpen(!isOpen);
+        console.log(isOpen)
+    };
+
+    const renderIngredients = () => {
+        return (
+            <ul>
+                {selectedIngredients?.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
+                ))}
+            </ul>
+        );
+    };
+
     return (
         <div className="notification-bell">
-            <FontAwesomeIcon icon={faBell} className="bell-icon" />
-            {count > 0 && <span className="notification-count">{count}</span>}
+            <div className="bell-icon" onClick={toggleNotifyBell}>
+                <FontAwesomeIcon icon={faBell} />
+                {count > 0 && (
+                    <div className="notification-count">{count}</div>
+                )}
+            </div>
+            <div className="notification-content">
+                <h3>Thông báo</h3>
+                {renderIngredients()}
+            </div>
         </div>
     );
 }
 
-export default NotificationBell;
+export default NotifyBell;
