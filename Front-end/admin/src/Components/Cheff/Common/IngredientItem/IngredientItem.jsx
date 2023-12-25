cc6528cfa946cd7d6fba5bfe24a312888168a075
nguyenquochuy753 <92136604+nguyenquochuy753@users.noreply.github.com>
2023-12-25 21:16:26 +0700

    update ingredient item page

--- Front-end/admin/src/Components/Cheff/Common/IngredientItem/IngredientItem.jsx
+++ Front-end/admin/src/Components/Cheff/Common/IngredientItem/IngredientItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import "./IngredientItem.css"
 
-const IngredientItem = ({ name, quantity, unit }) => {
+const IngredientItem = ({ name, quantity, unit, onClick }) => {
     return (
-        <div className="ingredient-item">
+        <div className="ingredient-item" onClick={onClick}>
             <h3>{name}</h3>
             <p>Quantity: {quantity} <b>{unit}</b></p>
         </div>
