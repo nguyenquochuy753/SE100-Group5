c57d05f03b7a0cbeeee2fee3f44b7b6726ccef4c
nguyenquochuy753 <92136604+nguyenquochuy753@users.noreply.github.com>
2023-12-26 01:16:52 +0700

    update ingredient page in cheff

--- Front-end/admin/src/Components/Cheff/Common/NotifyBell/NotifyBell.jsx
+++ Front-end/admin/src/Components/Cheff/Common/NotifyBell/NotifyBell.jsx
@@ -1,22 +1,66 @@
-import React, { useState } from 'react';
-import "./NotifyBell.css"
+import React, { useState, useEffect } from 'react';
+import './NotifyBell.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faPlus, faMinus, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 function NotifyBell({ count, selectedIngredients }) {
     const [isOpen, setIsOpen] = useState(false);
+    const [ingredientList, setIngredientList] = useState([]);
+
+    useEffect(() => {
+        if (selectedIngredients && selectedIngredients.length > 0) {
+            setIngredientList(selectedIngredients.map(ingredient => ({ name: ingredient, quantity: 1 })));
+        }
+    }, [selectedIngredients]);
 
     const toggleNotifyBell = () => {
         setIsOpen(!isOpen);
-        console.log(isOpen)
     };
+
+    const handleIncrease = (index) => {
+        const updatedList = [...ingredientList];
+        updatedList[index].quantity += 1;
+        setIngredientList(updatedList);
+    };
+
+    const handleDecrease = (index) => {
+        const updatedList = [...ingredientList];
+        if (updatedList[index].quantity > 1) {
+            updatedList[index].quantity -= 1;
+            setIngredientList(updatedList);
+        }
+    };
+
+    const handleRemoveIngredient = (indexToRemove) => {
+        const updatedList = [...ingredientList];
+        updatedList.splice(indexToRemove, 1);
+        setIngredientList(updatedList);
+        count = count - 1;
+    };
+
+    console.log(ingredientList)
 
     const renderIngredients = () => {
         return (
             <ul>
-                {selectedIngredients?.map((ingredient, index) => (
-                    <li key={index}>{ingredient}</li>
+                {ingredientList?.map((ingredient, index) => (
+                    <div className="ingredient-item1" key={index}>
+                        <span>{ingredient.name}</span>
+                        <div className="quantity-actions">
+                            <button onClick={() => handleDecrease(index)}>
+                                <FontAwesomeIcon icon={faMinus} />
+                            </button>
+                            <span className="quantity">{ingredient.quantity}</span>
+                            <button onClick={() => handleIncrease(index)}>
+                                <FontAwesomeIcon icon={faPlus} />
+                            </button>
+                            <button onClick={() => handleRemoveIngredient(index)}>
+                                <FontAwesomeIcon icon={faTimes} />
+                            </button>
+                        </div>
+                    </div>
                 ))}
+                <button className='confirm-btn'>Xác nhận</button>
             </ul>
         );
     };
@@ -29,8 +73,8 @@ function NotifyBell({ count, selectedIngredients }) {
                     <div className="notification-count">{count}</div>
                 )}
             </div>
-            <div className="notification-content">
-                <h3>Thông báo</h3>
+            <div className={`notification-content ${isOpen ? 'open' : ''}`}>
+                <h3>Giảm nguyên liệu</h3>
                 {renderIngredients()}
             </div>
         </div>
